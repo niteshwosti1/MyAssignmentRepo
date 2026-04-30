01a9756aed2b2f0ea7cfeeb290814cc20ea69ac0
Nitesh Wosti <niteshwosti1@gmail.com>
2026-04-30 18:05:20 -0400

    Health Check API endpoint added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
+//Function to validate user input
 function validateUserInput(body){
     // Check if the array is valid and not empty
     if (!Array.isArray(body) || body.length === 0) {
@@ -17,3 +19,32 @@ function validateUserInput(body){
     }
     return null;
 }
+// Function to calculate mean
+function calculateMean(numbers) {
+    let sum =0;
+   for (const num of numbers){
+        sum+= num;
+   }
+    return sum/numbers.length
+}
+//Function to calculate Standard Deviation
+function calculateStandardDeviation(numbers){
+   const mean = calculateMean(numbers);
+   let sumOfSqDiff = 0;
+    for(const num of numbers){
+        const SqDiff = num - mean;
+        sumOfSqDiff += SqDiff * SqDiff;
+    }
+    const variance = sumOfSqDiff/numbers.length;
+    return Math.sqrt(variance);
+}
+// Endpoint to check server health
+app.get('/health',(req,res)=>{
+    res.json({status:"Server is healthy", port: PORT});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
