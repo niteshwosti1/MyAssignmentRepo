33536319198975eb6b9a85a3e8cb0508233c5986
Nitesh Wosti <niteshwosti1@gmail.com>
2026-05-03 20:02:41 -0400

    Added additional validation

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({limit:'1mb'}));
+
+app.use((err, req, res, next) => {
+    if (err.type === 'entity.parse.failed'){
+        return res.status(400).json({
+            error:"Invalid JSON body"
+        });
+    }
+    next(err);
+})
 
 //Function to validate user input
 function validateUserInput(body){
@@ -11,14 +20,28 @@ function validateUserInput(body){
     if (!Array.isArray(body) || body.length === 0) {
         return "Invalid input: Expected a valid non-empty array.";
     }
-    //Check if the item in array is number or not
+    //Check if the item in array is number and is finite value.
     for (const item of body) {
         if (typeof item !== 'number' || Number.isNaN(item)) {
             return "Invalid input: Expected an array of numbers.";
         }
+        if (!Number.isFinite(item)) {
+            return "Invalid input: Array contains infinite value(s).";
+        }
     }
+    //Check if the body is udefined or null
+    if (body === undefined || body === null) {
+        return "Invalid input: Expected an array of numbers.";
+    }
+    //Check if the array length exceeds the maximum load limit set
+    const MAX_LENGTH = 100000;
+    if (body.length > MAX_LENGTH) {
+        return "Invalid input: Array length exceeds the maximum load limit";
+    }
+
     return null;
 }
+
 // Function to calculate mean
 function calculateMean(numbers) {
     let sum =0;
